6df85e643e03afd57e1482e6a8b1579d2553c570
AndraCimpean <andra.cimpean94@gmail.com>
2023-01-04 15:51:57 +0200

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+const encrypt = require ("mongoose-encryption"); 
 
 const app = express();
 
@@ -12,11 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true });
-//test editat
-const userSchema = { 
+
+const userSchema = new mongoose.Schema ({ 
     email:String, 
     password: String
-}; 
+});
+
+const secret = "thisisourlittlesecret."; 
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model ("User", userSchema);
 
@@ -40,7 +44,6 @@ app.post ("/register", function (req,res) {
     }); 
 
     newUser.save(function(err) { 
-
         if (err){ console.log(err); }
         else { res.render ("secrets");}
     });
@@ -51,7 +54,6 @@ app.post ("/register", function (req,res) {
 app.post ("/login", function (req,res) { 
     const username= req.body.username;
     const password= req.body.password;
-
     User.findOne({email:username}, function (err, foundUser){
 
         if (err) { 
