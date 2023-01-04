0c8d89bd35ddc26d5ba0faa32b89e2d6c89a9424
AndraCimpean <andra.cimpean94@gmail.com>
2023-01-04 14:45:21 +0200

    acesta este un commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true });
-
+//test editat
 const userSchema = { 
     email:String, 
     password: String
